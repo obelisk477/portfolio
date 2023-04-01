9b0b117102e58fdec813a570205c98beb64dcb8a
obelisk477 <cwbatte2@gmail.com>
2023-04-01 16:27:05 -0400

    made JS more concise

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,18 @@
 function carouselButton(buttonId) {
-    let otherButtons = []
-    if (buttonId === 1) {
-        otherButtons.push(2,3)
-    } else if (buttonId === 2) {
-        otherButtons.push(1,3)
-    } else {
-        otherButtons.push(2,1)
-    }
+    let otherButtons = buttonId === 1 ? [2,3] : buttonId === 2 ? [1,3] : [1,2]
 
-    var carouselItem = document.querySelector("#carousel-" + buttonId).style
-    var otherImgOne = document.querySelector("#carousel-" + otherButtons[0]).style
-    var otherImgTwo = document.querySelector("#carousel-" + otherButtons[1]).style
-    var buttonItem = document.querySelector("#button" + buttonId).style
-    var otherBtnOne = document.querySelector("#button" + otherButtons[0]).style
-    var otherBtnTwo = document.querySelector("#button" + otherButtons[1]).style
+    var carouselItemStyles = document.querySelector("#carousel-" + buttonId).style
+    var buttonItemStyles = document.querySelector("#button" + buttonId).style
 
-    carouselItem.height = "340px"
-    otherImgOne.height = otherImgTwo.height = "300px"
+    Object.assign(carouselItemStyles, {height: "340px", border: "solid 3px #BF5443", opacity: "1"})
+    buttonItemStyles.backgroundColor = "#BF5443"
+    
+    otherButtons.forEach((otherButtonId) => {
+        const otherCarouselItem = document.querySelector(`#carousel-${otherButtonId}`).style
+        const otherButtonItem = document.querySelector(`#button${otherButtonId}`).style
 
-    carouselItem.border = "solid 3px #BF5443"
-    otherImgOne.border = otherImgTwo.border = "none"
-
-    carouselItem.zIndex = "11"
-    otherImgOne.zIndex = "10"
-    otherImgTwo.zIndex = "9"
-
-    carouselItem.opacity = "1"
-    otherImgOne.opacity = otherImgTwo.opacity = "0.6"
-
-    buttonItem.backgroundColor = "#BF5443"
-    otherBtnOne.backgroundColor = otherBtnTwo.backgroundColor = "transparent"
+        Object.assign(otherCarouselItem, {height: "300px", border: "none", opacity: "0.4"})
+        Object.assign(otherButtonItem, {backgroundColor: "transparent"})
+    })
 
 }
